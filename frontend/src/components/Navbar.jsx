35e2d2b857a17864954b2f29c14ad688120dadd3
Eric Soon <eric.soonsmk@gmail.com>
2022-10-08 15:48:11 -0700

    added the navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <div className='container'>
+        <div className='logo'>
+          <img src="" alt="logo" />
+        </div>
+        <div className='links'>
+          <Link to ="?cat=art"className='link'>
+            <h6>Art</h6>
+          </Link>
+          <Link to ="?cat=art"className='link'>
+            <h6>Science</h6>
+          </Link>
+          <Link to ="?cat=art"className='link'>
+            <h6>Cinema</h6>
+          </Link>
+          <Link to ="?cat=art"className='link'>
+            <h6>Design</h6>
+          </Link>
+          <Link to ="?cat=art"className='link'>
+            <h6>Food</h6>
+          </Link>
+          <span>John</span>
+          <span>Logout</span>
+          <span className='write'>
+            <Link className= "link" to="/write">Write</Link>
+          </span>
+        </div>
+      </div>
+    </div>
   )
 }
 
