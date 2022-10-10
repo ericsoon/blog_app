bebca0dd24fe7e04f748e13f96c3288f272bcf08
Eric Soon <eric.soonsmk@gmail.com>
2022-10-10 16:20:40 -0700

    implementing the endpoint from express server to the register page

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const Register = () => {
-  console.log('a');
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '',
+    password: '',
+
+  });
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await axios.post('/auth/register', inputs);
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  // console.log(inputs);
   return (
     <div className="auth">
       <h1>Register</h1>
       <form>
-        <input required type="text" placeholder="username" />
-        <input required type="email" placeholder="email" />
-        <input required type="password" placeholder="password" />
-        <button type="button">Register</button>
+        <input required type="text" placeholder="username" name="username" onChange={handleChange} />
+        <input required type="email" placeholder="email" name="email" onChange={handleChange} />
+        <input required type="password" placeholder="password" name="password" onChange={handleChange} />
+        <button type="button" onClick={handleSubmit}>Register</button>
         <p>This is an error!</p>
         <span>Create a new account? <Link to="/register">Register</Link></span>
       </form>
