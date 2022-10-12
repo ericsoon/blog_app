2bc8ead75447cc1fa6b5107232e834b75383bf43
Eric Soon <eric.soonsmk@gmail.com>
2022-10-11 20:28:28 -0700

    fetching the data from getpost endpoint

--- frontend/src/pages/Single.jsx
+++ frontend/src/pages/Single.jsx
@@ -1,25 +1,49 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect, useContext } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import axios from 'axios';
+import moment from 'moment';
 
 import Edit from '../img/edit.png';
 import Delete from '../img/delete.png';
 import { Menu } from '../components';
+import { AuthContext } from '../context/authContext';
 
 const Single = () => {
-  console.log('a');
+  const [post, setPost] = useState([]);
+
+  const location = useLocation();
+
+  const postId = location.pathname.split('/')[2];
+
+  const { currentUser } = useContext(AuthContext);
+
+  // console.log(cat);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`/posts/${postId}`);
+        setPost(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, [postId]);
   return (
     <div className="single">
       <div className="content">
-        <img src="" alt="content" />
+        <img src={post?.img} alt="content" />
 
         <div className="user">
           <img src="" alt="user" />
 
           <div className="info">
-            <span>John</span>
-            <p>Posted 2 days ago</p>
+            <span>{post.username}</span>
+            <p>Posted {moment(post.date).fromNow()}</p>
           </div>
 
+          {currentUser.username === post.username && (
           <div className="edit">
             <Link to="/write/?edit=2">
               <img src={Edit} alt="edit" />
@@ -28,36 +52,11 @@ const Single = () => {
               <img src={Delete} alt="delete" />
             </Link>
           </div>
+          )}
         </div>
 
-        <h1>Lorem ipsum dolor</h1>
-        <p>
-          Sed ut perspiciatis, unde omnis iste natus error sit voluptatem ac
-          cusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae
-          ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt,
-          explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut o
-          dit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptat
-          em sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit
-          , amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora in
-          cidunt, ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad mini
-          ma veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nis
-          i ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit,
-          qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum,
-          qui dolorem eum fugiat, quo voluptas nulla pariatur? [33] At vero eos et
-          accusamus et iusto odio dignissimos ducimus, qui blanditiis praesentium v
-          oluptatum deleniti atque corrupti, quos dolores et quas molestias exceptu
-          ri sint, obcaecati cupiditate non provident, similique sunt in culpa, qui
-          officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum
-          quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum
-          soluta nobis est eligendi optio, cumque nihil impedit, quo minus id, q
-          uod maxime placeat, facere possimus, omnis voluptas assumenda est, omni
-          s dolor repellendus. Temporibus autem quibusdam et aut officiis debitis
-          aut rerum necessitatibus saepe eveniet, ut et voluptates repudiandae s
-          int et molestiae non recusandae. Itaque earum rerum hic tenetur a sapi
-          ente delectus, ut aut reiciendis voluptatibus maiores alias consequatu
-          r aut perferendis doloribus asperiores
-          repellat.
-        </p>
+        <h1>{post.title}</h1>
+        {post.desc}
       </div>
       <Menu />
     </div>
