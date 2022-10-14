aa85ea4ec268a2077761e4efdc3f85d42ca69fc2
Eric Soon <eric.soonsmk@gmail.com>
2022-10-13 19:23:33 -0700

    fixed a bug (now images that have been uploaded have the file type

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,16 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-const upload = multer({ dest: 'uploads/' });
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, 'uploads');
+  },
+  filename(req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
 
 app.post('/api/upload', upload.single('file'), (req, res) => {
   res.status(200).json('Image has been uploaded');
