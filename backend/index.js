eeec7790b7a523b138b3bb32943587b5657cf0c0
Eric Soon <eric.soonsmk@gmail.com>
2022-10-13 19:13:10 -0700

    added functions on write component and added multer library

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import multer from 'multer';
 
 import authRoutes from './routes/auth.js';
 import userRoutes from './routes/users.js';
@@ -9,6 +10,13 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
+const upload = multer({ dest: 'uploads/' });
+
+app.post('/upload', upload.single('file'), (req, res) => {
+  res.status(200).json('Image has been uploaded');
+});
+
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
